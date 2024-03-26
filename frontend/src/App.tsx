343871b172c8e7f68cd15f65237a39d7ba527440
Pranav Avva <pranav.avva@gmail.com>
2024-03-25 20:23:33 -0400

    layout mui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import { DataGrid } from "@mui/x-data-grid";
+import { useDemoData } from "@mui/x-data-grid-generator";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const { data } = useDemoData({
+    dataSet: "Commodity",
+    rowLength: 5,
+    maxColumns: 6,
+  });
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <Container>
+        <Box sx={{ my: 4 }}>
+          <Typography variant="h4" component="h1" gutterBottom>
+            BoreDM Full Stack Developer Final Challenge
+          </Typography>
 
-export default App
+          <DataGrid {...data} />
+        </Box>
+      </Container>
+    </>
+  );
+}
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true,
+    port: 5173,
+    open: true,
+    proxy: {
+      "/api": {
+        target: "http://0.0.0.0:5000",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path: string) => path.replace(/^\/api/, ""),
+      },
+      "/ws": {
+        target: "ws://0.0.0.0:5000",
+        ws: true,
+      },
+    },
+  },
+});
