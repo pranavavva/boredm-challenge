6712a944e9cee2c963d61d67286047424c5f2fa0
Pranav Avva <pranav.avva@gmail.com>
2024-03-26 04:42:17 -0400

    dockerization almost done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+// import * as React from "react";
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
